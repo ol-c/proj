46c5a2e7d8e5a5d92df5c71a4bf3339b30d29e6f
ol-c <fordjason@gmail.com>
2014-05-20 16:54:58 -0700

    hashmap renderer

--- static/renderers/jquery.render.hashmap.js
+++ static/renderers/jquery.render.hashmap.js
@@ -5,57 +5,63 @@ $.fn.render.hashmap = function (item, after) {
         fontFamily : 'monospace',
     });
 
+    var rendered_fields = {};
+
+    function render_field(key, reference, after) {
+        //  TODO: show loader
+        var field = $('<span>').text('"' + key.replace('"', '\\"') + '"');
+        var divider = $('<span> : </span>');
+        divider.css({
+            color : '#888888'
+        });
+        var value = $('<span>');
+        var row = $('<div>');
+        row.append([field, divider, value, after]);
+        $(field).add(divider).add(value).css({
+            padding : 0,
+            borderSpacing : 0,
+        });
+        var key_width = 5 + key.length;
+        field.css({
+            color : 'lime',
+            marginLeft : (-key_width) + 'ex'
+        });
+        row.css({
+            paddingLeft : (4 + key_width) +'ex'
+        });
+
+        perform_operation({
+            type : 'source reference',
+            reference : reference
+        }, function (item) {
+            value.render(item, after);
+        });
+        
+        rendered_fields[key] = row;
+
+        return row;
+    }
+
+    var content_body = $('<div>');
     function render_hashmap() {
         var open = $('<span>{</span>');
         var command_line = $('<span>');
         command_line.command(item, user_context(item));
         open.append(command_line)
-        var content_body = $('<div>');
         var close = $('<span>}</span>');
         var keys = Object.keys(item.data);
 
-        function render(container, reference, end) {
-            //  TODO: show loader
-            perform_operation({
-                type : 'source reference',
-                reference : reference
-            }, function (item) {
-                container.render(item, end);
-            });
-        }
-
         for (var i=0; i<keys.length; i++) {
             var key = keys[i];
-            var field = $('<span>').text('"' + key.replace('"', '\\"') + '"');
-            var divider = $('<span> : </span>');
-            divider.css({
-                color : '#888888'
-            });
-            var value = $('<span>');
             var terminate = undefined;
-            
             if (i < keys.length - 1) {
                 terminate = $('<span>,</span>');
                 terminate.css({
                     color : '#888888'
                 });
             }
-            render(value, item.data[key], terminate);
-            var row = $('<div>');
-            row.append([field, divider, value]);
+            var row = render_field(key, item.data[key], terminate);
             content_body.append(row);
-            $(field).add(divider).add(value).css({
-                padding : 0,
-                borderSpacing : 0,
-            });
-            var key_width = 5 + key.length;
-            field.css({
-                color : 'lime',
-                marginLeft : (-key_width) + 'ex'
-            });
-            row.css({
-                paddingLeft : (4 + key_width) +'ex'
-            });
         }
         rendered = $('<span>');
         $(rendered).append([open, content_body, close]);
@@ -65,12 +71,28 @@ $.fn.render.hashmap = function (item, after) {
     $(this).append(container);
     render_hashmap();
 
+
     function watch_fn(update) {
         if (update.value.type == 'hashmap') {
-            console.log('HASHMAP UPDATED!!!!', update);
+            var updated_fields = update.value.data;
+            var field;
+            for (field in updated_fields) {
+                if (rendered_fields[field] === undefined) {
+                    var rendered = render_field(field, updated_fields[field]);
+                    content_body.prepend(rendered);
+                }
+            }
+            for (field in rendered_fields) {
+                if (updated_fields[field] === undefined) {
+                    rendered_fields[field].remove();
+                    delete rendered_fields[field];
+                }
+            }
         }
         else {
-            console.log('uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuhhhhhhh')
+            //  this should never be called, as a persistant type
+            //  cannot change its own type... hmmm, or should it
+            //  be able to?!
             self.empty();
             self.render(update.value, after);
             unwatch(item.reference, watch_fn);
